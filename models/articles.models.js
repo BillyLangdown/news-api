404420243523c20f4f2b7a71014c317edcd399cf
BillyLangdown <billy.langdown01@gmail.com>
2024-01-16 09:48:57 -0500

    complete 05-get-articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -13,3 +13,17 @@ exports.fetchArticleById = (article_id) => {
       return rows;
     });
 };
+
+exports.fetchArticles = () => {
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id) AS comment_count
+      FROM articles
+      JOIN comments ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
